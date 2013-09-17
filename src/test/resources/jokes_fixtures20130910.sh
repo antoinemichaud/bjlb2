2c916d9bdb6a9d44306369b7a52d7cf8a30cf6c8
Nicolas Guignard <nicolas065@gmail.com>
2013-09-17 16:37:08 +0200

    add of some jokes in the script with good dates

--- src/test/resources/jokes_fixtures20130910.sh
+++ src/test/resources/jokes_fixtures20130910.sh
@@ -30,5 +30,56 @@ curl -v -i -XPUT http://localhost:10018/riak/publications/20130915_regular -H "C
 "authorMail":"tasoeur@jokinette.com", "publicationDate":"15/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
 
 
-curl -v -i -XPUT http://localhost:10018/riak/publications/20130916_regular -H "Content-Type: application/json" -d '{"previousJokeDate":"20130915","authorName":"Tonperer",
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130916_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130917", "previousJokeDate":"20130915","authorName":"Tonperer",
+"authorMail":"tonpere@jokinette.com", "publicationDate":"16/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130917_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130918", "previousJokeDate":"20130916","authorName":"boloss",
+"authorMail":"boloss@jokinette.com", "publicationDate":"08/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130918_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130919", "previousJokeDate":"20130917","authorName":"Nico",
+"authorMail":"boloss@jokinette.com", "publicationDate":"09/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130919_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130920", "previousJokeDate":"20130918","authorName":"Antoine",
+"authorMail":"antoineo@jokinette.com", "publicationDate":"10/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130920_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130921", "previousJokeDate":"20130919","authorName":"paulo",
+"authorMail":"paulo@jokinette.com", "publicationDate":"11/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130921_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130922", "previousJokeDate":"20130920","authorName":"vivi",
+"authorMail":"vivi@jokinette.com", "publicationDate":"12/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130922_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130923", "previousJokeDate":"20130921","authorName":"tivi",
+"authorMail":"tivi@jokinette.com", "publicationDate":"13/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130923_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130924", "previousJokeDate":"20130922","authorName":"Tutu",
+"authorMail":"tutu@jokinette.com", "publicationDate":"14/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130924_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20130925", "previousJokeDate":"20130923","authorName":"Tusoeur",
+"authorMail":"tasoeur@jokinette.com", "publicationDate":"15/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20130925_regular -H "Content-Type: application/json" -d '{"previousJokeDate":"20130924","authorName":"Tonperer",
 "authorMail":"tonpere@jokinette.com", "publicationDate":"16/09/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
