125f42d7f62c19e35fa9d37548dd4e60e7828e19
Antoine MICHAUD <antoinemichaud34@gmail.com>
2013-09-21 10:10:52 +0200

    Merge db rollbacks

--- src/test/resources/jokes_fixtures20130910_rollback.sh
+++ src/test/resources/jokes_fixtures20130910_rollback.sh
@@ -2,3 +2,14 @@ curl -v -i -XDELETE http://localhost:10018/riak/publications/20130728_regular
 curl -v -i -XDELETE http://localhost:10018/riak/publications/20130729_regular
 curl -v -i -XDELETE http://localhost:10018/riak/publications/20130730_regular
 curl -v -i -XDELETE http://localhost:10018/riak/publications/20130804_regular
+
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130908_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130909_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130910_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130911_regular
+
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130912_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130913_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130914_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130915_regular
+curl -v -i -XDELETE http://localhost:10018/riak/publications/20130916_regular
