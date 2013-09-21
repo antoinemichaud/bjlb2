8193407a6af2cf5104e47937a95839499ea4d9e2
Antoine MICHAUD <antoinemichaud34@gmail.com>
2013-09-21 12:42:44 +0200

    Refacto

--- src/main/java/com/nextgestion/bjlb/repository/JokesRepository.java
+++ src/main/java/com/nextgestion/bjlb/repository/JokesRepository.java
@@ -1,18 +1,12 @@
 package com.nextgestion.bjlb.repository;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.logging.Logger;
-
 import com.basho.riak.client.IRiakClient;
 import com.basho.riak.client.RiakRetryFailedException;
 import com.basho.riak.client.bucket.Bucket;
 import com.nextgestion.bjlb.model.Joke;
 import com.nextgestion.bjlb.storedtype.Publication;
+
+import java.util.logging.Logger;
 
 public class JokesRepository {
 
@@ -26,68 +20,25 @@ public class JokesRepository {
         this.riakClient = riakClient;
     }
 
-    public Joke findJoke(String date) {
+    public Joke findJoke(String requestedDate) {
+        Joke joke = null;
+
         try {
             final Bucket bucket = riakClient.fetchBucket(PUBLICATIONS).execute();
 
-            final String dateStr = date + "_regular";
+            final String dateStr = requestedDate + "_regular";
             final Publication publication = bucket.fetch(dateStr, Publication.class).execute();
+
             if (publication != null) {
-            	final Joke joke = new Joke(publication.getPreviousJokeDate(), publication.getNextJokeDate(),
+                joke = new Joke(publication.getPreviousJokeDate(), publication.getNextJokeDate(),
                         publication.getAuthorName(), publication.getAuthorName(), Joke.Category.BAD,
                         publication.getPublicationDate(), publication.getJokeContent());
-            	
-            	return joke;
+
             }
         } catch (RiakRetryFailedException e) {
-        	logger.severe("Erreur lors de la communication avec le bucket publications");
-        } 
-        
-        // TODO 
-        // creation dans le futur d'une classe utilitaire regroupant tous ces bouts de codes inutiles 
-        // et pas beaux
-        final Date defaultDate = new Date();
-        final DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        final String defaultDateStr = dateFormat.format(defaultDate);
-        
-        boolean isDefaultDateIsNotPresent = false;
-        if (!date.equals(defaultDateStr)){
-        	isDefaultDateIsNotPresent = true;
-        } else {
-        	isDefaultDateIsNotPresent = false;
+            logger.severe("Erreur lors de la communication avec le bucket publications");
         }
-        
-        return findDefaultJoke(defaultDateStr, isDefaultDateIsNotPresent);
+
+        return joke;
     }
-    
-    /**
-     * method when we don't have in database the joke of the asked day. And we are even processing
-     * when we don't have the joke of today.
-     * @param dateStr
-     * @param isDefaultDateIsNotPresent
-     * @return
-     */
-	public Joke findDefaultJoke(final String dateStr, final boolean isDefaultDateIsNotPresent) {
-
-		if (isDefaultDateIsNotPresent) {
-			final DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-			Date reverseDate = null;
-			try {
-				reverseDate = dateFormat.parse(dateStr);
-			} catch (ParseException e) {
-				logger.severe("Erreur lors du parsing de notre Date String en Date - findDefaultJoke method");
-			}
-			Calendar calendar = new GregorianCalendar();
-			calendar.setTime(reverseDate);
-			calendar.add(Calendar.DATE, -1);
-			final Date newDate = calendar.getTime();
-			
-			return findJoke(dateFormat.format(newDate));
-		
-		} else {
-			return findJoke(dateStr);
-		}
-
-	}
-    
 }
--- src/main/java/com/nextgestion/bjlb/service/PageContentService.java
+++ src/main/java/com/nextgestion/bjlb/service/PageContentService.java
@@ -1,16 +1,19 @@
 package com.nextgestion.bjlb.service;
 
-import java.util.logging.Logger;
-
-import org.joda.time.DateTime;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nextgestion.bjlb.model.Joke;
 import com.nextgestion.bjlb.repository.JokesRepository;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.logging.Logger;
 
 public class PageContentService {
 
     private static final Logger logger = Logger.getAnonymousLogger();
+    public static final String BJLB_DATE_FORMAT = "yyyyMMdd";
 
     private static ObjectMapper mapper = new ObjectMapper();
 
@@ -21,26 +24,40 @@ public class PageContentService {
     }
 
     public String getPageContent(String date) {
-        try {
-            final String jsonValue = mapper.writeValueAsString(jokesRepository.findJoke(date));
+        Joke joke = jokesRepository.findJoke(date);
+        return formattedJoke(joke);
+    }
 
-            return jsonValue;
+    public String getPageContent() {
+        final DateTime dateTime = DateTime.now();
+        final int formattedIntegerDate = dateTime.getYear() * 10_000 + dateTime.getMonthOfYear() * 100 + dateTime.getDayOfMonth();
+        final Joke closestJoke = findClosestJoke(Integer.toString(formattedIntegerDate));
+
+        return formattedJoke(closestJoke);
+    }
+
+    private Joke findClosestJoke(final String dateStr) {
+        final DateTimeFormatter formatter = DateTimeFormat.forPattern(BJLB_DATE_FORMAT);
+        final DateTime requestedDateTime = DateTime.parse(dateStr, formatter);
+
+        Joke joke = null;
+        int counter = 0;
+        while (joke == null && counter < 100) {
+            joke = jokesRepository.findJoke(requestedDateTime.toString(BJLB_DATE_FORMAT));
+            counter++;
+            requestedDateTime.minusDays(1);
+        }
+
+        return joke;
+    }
+
+    private String formattedJoke(Joke joke) {
+        try {
+            return mapper.writeValueAsString(joke);
         } catch (JsonProcessingException e) {
             logger.severe("Erreur lors du parsing du fichier : \n" + e);
             return "Error in the database";
         }
     }
 
-    /**
-     * 
-     * method we call when we don't ask with a date so it's when we want the date of today
-     * @return
-     */
-    public String getPageContent() {
-        final DateTime dateTime = DateTime.now();
-        final int formattedIntegerDate = dateTime.getYear() * 10_000 + dateTime.getMonthOfYear() * 100 + dateTime.getDayOfMonth();
-
-        final String pageContent = getPageContent(Integer.toString(formattedIntegerDate));
-        return pageContent;
-    }
 }
