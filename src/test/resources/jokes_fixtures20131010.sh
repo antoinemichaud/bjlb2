567789a9d24c5d04ff3ed2bc9a5fa9dbb5039c3d
Nicolas Guignard <nicolas065@gmail.com>
2013-10-10 17:54:49 +0200

    new scripts pour la BDD Riak

---
+++ src/test/resources/jokes_fixtures20131010.sh
@@ -0,0 +1,68 @@
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131008_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131009", "previousJokeDate":"20131007","authorName":"boloss",
+"authorMail":"boloss@jokinette.com", "publicationDate":"08/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131009_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131010", "previousJokeDate":"20131008","authorName":"Nico",
+"authorMail":"boloss@jokinette.com", "publicationDate":"09/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131010_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131011", "previousJokeDate":"20131009","authorName":"Antoine",
+"authorMail":"antoineo@jokinette.com", "publicationDate":"10/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131011_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131012", "previousJokeDate":"20131010","authorName":"paulo",
+"authorMail":"paulo@jokinette.com", "publicationDate":"11/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131012_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131013", "previousJokeDate":"20131011","authorName":"vivi",
+"authorMail":"vivi@jokinette.com", "publicationDate":"12/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131013_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131014", "previousJokeDate":"20131012","authorName":"tivi",
+"authorMail":"tivi@jokinette.com", "publicationDate":"13/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131014_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131015", "previousJokeDate":"20131013","authorName":"Tutu",
+"authorMail":"tutu@jokinette.com", "publicationDate":"14/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131015_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131016", "previousJokeDate":"20131014","authorName":"Tusoeur",
+"authorMail":"tasoeur@jokinette.com", "publicationDate":"15/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131016_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131017", "previousJokeDate":"20131015","authorName":"Tonperer",
+"authorMail":"tonpere@jokinette.com", "publicationDate":"16/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131017_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131018", "previousJokeDate":"20131016","authorName":"boloss",
+"authorMail":"boloss@jokinette.com", "publicationDate":"08/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131018_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131019", "previousJokeDate":"20131017","authorName":"Nico",
+"authorMail":"boloss@jokinette.com", "publicationDate":"10/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131019_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131020", "previousJokeDate":"20131018","authorName":"Antoine",
+"authorMail":"antoineo@jokinette.com", "publicationDate":"10/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131020_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131021", "previousJokeDate":"20131019","authorName":"paulo",
+"authorMail":"paulo@jokinette.com", "publicationDate":"11/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131021_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131022", "previousJokeDate":"20131020","authorName":"vivi",
+"authorMail":"vivi@jokinette.com", "publicationDate":"12/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131022_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131023", "previousJokeDate":"20131021","authorName":"tivi",
+"authorMail":"tivi@jokinette.com", "publicationDate":"13/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131023_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131024", "previousJokeDate":"20131022","authorName":"Tutu",
+"authorMail":"tutu@jokinette.com", "publicationDate":"14/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131024_regular -H "Content-Type: application/json" -d '{"nextJokeDate":"20131025", "previousJokeDate":"20131023","authorName":"Tusoeur",
+"authorMail":"tasoeur@jokinette.com", "publicationDate":"15/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
+
+
+curl -v -i -XPUT http://localhost:10018/riak/publications/20131025_regular -H "Content-Type: application/json" -d '{"previousJokeDate":"20131024","authorName":"Tonperer",
+"authorMail":"tonpere@jokinette.com", "publicationDate":"16/10/2013", "category":"regular", "jokeContent":"No doubt, this is a really good joke"}'
